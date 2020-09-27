43c7cfbe46297b92537680aacf93096a909a3e89
Wei Lun <weilun_95@hotmail.com>
2020-09-27 13:04:35 +0800

    add fiber version

---
+++ cmd/version.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fiber-cli/pkg/fibercli"
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "output the fiber version number",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		latestVersion, err := fibercli.ReleaseVersion()
+		if err != nil {
+			return err
+		}
+
+		wd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Latest fiber release: %s\n", latestVersion)
+
+		currentVersion, err := fibercli.CurrentVersion(wd)
+		if err != nil {
+			fmt.Printf("Error in getting current Fiber version: %s", err)
+		}
+
+		fmt.Printf("Current fiber release: %s\n", currentVersion)
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
