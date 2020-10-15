208a9f6fc0d3fa375846a07ac49473ac4c3ed181
Tom <tom@tdpain.net>
2020-10-15 18:51:27 +0100

    📦 Add version check and warning when a new version exists

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/mattn/go-colorable"
+	"github.com/mattn/go-isatty"
 	"github.com/spf13/cobra"
 )
 
@@ -22,6 +25,7 @@ var osExit = os.Exit
 
 var cliVersion string
 var cliLatestVersion string
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute(version string) {
@@ -40,5 +44,27 @@ func Execute(version string) {
 	if err := rootCmd.Execute(); err != nil {
 		rootCmd.Println(err)
 		osExit(1)
+	}
+
+	const (
+		// cBlack = "\u001b[90m"
+		// cRed   = "\u001b[91m"
+		// cCyan = "\u001b[96m"
+		// cGreen = "\u001b[92m"
+		cYellow = "\u001b[93m"
+		// cBlue    = "\u001b[94m"
+		// cMagenta = "\u001b[95m"
+		// cWhite   = "\u001b[97m"
+		cReset = "\u001b[0m"
+	)
+
+	out := colorable.NewColorableStdout()
+	if os.Getenv("TERM") == "dumb" || (!isatty.IsTerminal(os.Stdout.Fd()) && !isatty.IsCygwinTerminal(os.Stdout.Fd())) {
+		out = colorable.NewNonColorable(os.Stdout)
+	}
+
+	if cliVersion != cliLatestVersion {
+		fmt.Fprintf(out, "\n%sWARNING: You are using fiber-cli version %s; however, version %s is available.\n", cYellow, cliVersion, cliLatestVersion)
+		fmt.Fprintf(out, "You should consider upgrading via the 'go get -u github.com/gofiber/fiber-cli' command.%s", cReset)
 	}
 }
