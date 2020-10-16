4abaee3187af140793cabc965bc49ef78ce54749
Tom <tom@tdpain.net>
2020-10-16 09:26:45 +0100

    📦 And another small tweak

--- cmd/update.go
+++ cmd/update.go
@@ -20,7 +20,7 @@ func upgradeRunE(cmd *cobra.Command, _ []string) error {
 	update := execCommand("go", "get", "-u", "-v", "github.com/gofiber/fiber-cli")
 	if _, err := update.CombinedOutput(); err != nil {
 		cmd.Printf("fiber upgrade: failed to update: %s\nCheck the logs for more info.\n", err)
-		return err
+		return nil
 	}
 
 	cmd.Printf("fiber upgrade: updated %s -> %s successfully!\n", version, cliLatestVersion)
